1dd4889927afbeae260911472be40b0d34510359
aaaaatoz <rafa.xu.au@gmail.com>
2015-11-20 22:37:16 +1100

    add ebs

---
+++ EC2/getEBS.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+#delete the previous snapshot
+#create the snapshot
+#make sure the snapshot is created
+import boto3
+import sys
+import datetime
+
+def main():
+	print "in-use volumes:"
+	print "create date\t\tvolume name"
+	getVolumes('in-use')
+	print "available volumes:"
+	print "create date\t\tvolume name"
+	getVolumes('available')
+
+def getVolumes(status):
+	ec2 = boto3.resource('ec2')
+	volumes = ec2.volumes.filter(
+    	Filters=[{'Name': 'status', 'Values': [status]}])
+	for volume in volumes:
+		print "%s\t%s" %(volume.create_time.strftime("%Y-%m-%D"), volume.volume_id)
+
+
+def usage():
+	print "./getEBS.py"
+
+if __name__ == "__main__":
+	if len(sys.argv) != 1 :
+		usage()
+		sys.exit(-1)
+	main()
--- EC2/getEC2instance.py
+++ EC2/getEC2instance.py
@@ -8,6 +8,7 @@ import sys
 import datetime
 
 def main():
+	ec2 = boto3.resource('ec2')
 	instances = ec2.instances.filter(
 	    Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
 	for instance in instances:
